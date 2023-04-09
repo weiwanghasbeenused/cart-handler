f8d5c55d0d72badb2434047bec002c6cd4766609
wei <wei@o-r-g.com>
2023-04-09 14:56:41 +0800

    update routing

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ if($uri[1] == 'api'){
 	require_once('config/config.php');
 	require_once('response/'.$uri[2].'.php');
 } 
-else{
+else if(!$uri[1]){
 	require_once('views/head.php');
-	if( !$uri[1] ) require_once('views/main.php');
-	else require_once('views/404.php');
+	require_once('views/main.php');
+	// else require_once('views/404.php');
 	require_once('views/foot.php');
 }
 
--- response/products.php
+++ response/products.php
@@ -1,6 +1,7 @@
 <?
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
+require_once('../config/config.php');
 $db = db_connect('guest');
 $sql = 'SELECT * FROM products';
 $res = $db->query($sql);
@@ -9,5 +10,5 @@ while($obj = $res->fetch_assoc()){
 	$products[] = $obj;
 }
 echo json_encode($products, JSON_UNESCAPED_UNICODE);
-
+exit();
 ?>
