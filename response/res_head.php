3218a4316a48356ae3e055e8eb485680a103ceaa
wei <wei@o-r-g.com>
2023-09-19 08:15:33 +0800

    update task message for api post pages

--- response/res_head.php
+++ response/res_head.php
@@ -3,7 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=utf-8");
 
 if(empty($_POST)) {
-        echo '這個檔案只負責處理註冊, 不顯示任何內容';
+        $task = $uri[2] == 'cartHandler' ? '接收購物車資料' : '處理註冊';
+        echo '這個檔案只負責' . $task . ', 不顯示任何內容';
         exit();
 }
 $db = db_connect('main');
